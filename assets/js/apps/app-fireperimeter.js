6c7f02d3291842e46d0d6ae352cddac5551e9b4b
shadowkyros <randall.vowles@utah.edu>
2016-11-30 08:51:25 -0700

    dfp, bfp, and sort function added

--- assets/js/apps/app-fireperimeter.js
+++ assets/js/apps/app-fireperimeter.js
@@ -22,12 +22,15 @@
     };
     // d3.json("http://home.chpc.utah.edu/~u0540701/fireserver/sample_fire2.json", function(data){
     var stidStack = [];
+    var stidAndDist = [];
     var key;
     for (key in sample_fire.nearest_stations) {
         stidStack.push(sample_fire.nearest_stations[key]["STID"]);
+        stidAndDist.push(sample_fire.nearest_stations[key]["DFP"]);
+
     };
 
-    // console.log(stidStack);
+    // console.log(stidAndDist);
     var stidList = stidStack.join(",");
     // console.log(stidList);
     apiArgs.stid = stidList;
@@ -35,28 +38,13 @@
         api_args: apiArgs
     });
     var filter = JSON.parse(M.windowArgs().select)
-    console.log(filter);
-    console.log(Object.keys(filter).length);
+    // console.log(filter);
+    // console.log(Object.keys(filter).length);
 
     M.printResponse();
     $.when(M.async()).done(function () {
         _networkTableEmitter(M, tableArgs);
         _highlightCells(filter);
-        //     _highlightCells({
-        //     "selector": ".air_temp",
-        //     "min": 40,
-        //     "max": 60
-        // }),
-        //     _highlightCells({
-        //     "selector": ".relative_humidity",
-        //     "min": 20,
-        //     "max": 50
-        // }),
-        // _highlightCells({
-        //     "selector": ".wind_speed",
-        //     "min": 10,
-        //     "max": undefined
-        // })
     });
 
 
@@ -77,10 +65,13 @@
         // we generate the table correctly.  We also want an array to put our sorted keys
         // back in to.  Once the sensors are ranked, we will create a sorted output that
         // will be ready to generate a table from.
-        rankedSensors.splice(0, 0, "date_time")
+        rankedSensors.splice(0,0, "dfp")
+        
+        rankedSensors.splice(1,0, "bfp")
+        rankedSensors.splice(2,0, "date_time")
         var stations = [];
         var i = 0;
-        var l = _s.length
+        var l = _s.length;
         while (i < l) {
             // We need to find the last element in the array, since that should be the most
             // current for the text range. Then we populate it with key/value pairs that 
@@ -93,12 +84,20 @@
 
             var last = _s[i].OBSERVATIONS.date_time.length - 1
             var tmp = {};
-            tmp.stid = _s[i].STID
+            tmp.stid = _s[i].STID;
 
             rankedSensors.map(function (d) {
+                // console.log(d)
+                // console.log(stidAndDist[i][0])
                 // Best to use terinary logic here, but for simplicity...
-                if (typeof _s[i].OBSERVATIONS[d === "date_time" ? d : d + "_set_1"] === "undefined") {
+                if (d === "bfp" || d === "dfp") {
+                    tmp[d] = (stidAndDist[i][0]).toFixed(2);
+                    tmp[d] = (stidAndDist[i][1]).toFixed(2);
+                    
+                } else if (typeof _s[i].OBSERVATIONS[d === "date_time" ? d : d + "_set_1"] === "undefined") {
                     tmp[d] = null;
+                
+
                 } else {
                     tmp[d] = _s[i].OBSERVATIONS[d === "date_time" ? d : d + "_set_1"][last]
                 }
@@ -123,6 +122,39 @@
             .selectAll("th").data(["stid"].concat(rankedSensors)).enter().append("th")
             .html(function (d) {
                 return d;
+            })
+            .attr("id", function (d) { return d; })
+            .classed("table-header", true)
+            .property("sorted", false)
+            .on('click', function (d) {
+
+                var _thisId = d3.select(this).attr("id");
+                var _this = this;
+                var _state = d3.select(this).property("sorted");
+                d3.select(_this).property("sorted", function (d) { return _state ? false : true; });
+
+                if (_thisId !== "date_time") {
+                    rows.sort(function (a, b) {
+                        // Typeguarding for null values.                   
+                        var _a = a[d] === null ? -9999 : typeof a[d] === "object" ? a[d][0] : a[d];
+                        var _b = b[d] === null ? -9999 : typeof b[d] === "object" ? b[d][0] : b[d];
+                        return _state ? _a - _b : _b - _a;
+                    });
+                } else if (_thisId === "stid"){
+                    rows.sort(a, b); 
+                };
+                
+        
+                d3.selectAll(".table-header").selectAll("i").classed("fa-chevron-circle-down", false);
+                d3.selectAll(".table-header").selectAll("i").classed("fa-chevron-circle-up", false);
+
+                d3.select("#" + _thisId).select("i")
+                    .classed("fa-chevron-circle-up", function () { return _state ? true : false; })
+                    .classed("fa-chevron-circle-down", function () { return !_state ? true : false; });
+            })
+            .append("i").attr("class", "sort-icon fa")
+            .classed("fa-chevron-circle-down", function (d) {
+                return d === "dfp" ? true : false;
             });
 
         // Create the rows
